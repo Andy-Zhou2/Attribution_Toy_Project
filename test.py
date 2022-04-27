42086701377a17e38292bb60fbaf0affd7cbbeb8
Andy Zhou <wz337@cam.ac.uk>
2022-04-27 09:44:54 +0100

    use randomly generated matrix to test - more unstable behavior; next step: masking

--- test.py
+++ test.py
@@ -5,14 +5,18 @@ import numpy as np
 
 net = TraceNet(10)
 
-matrix = t.tensor([[1, 2], [3, 4]], dtype=t.float)
-# matrix *= 10
-correct_trace = matrix[0][0] + matrix[1][1]
+# matrix = t.tensor([[1, 2], [3, 4]], dtype=t.float)
+# # matrix *= 10
+# correct_trace = matrix[0][0] + matrix[1][1]
+matrix, correct_trace = get_random_matrix_trace(10)
+correct_trace = correct_trace.item()
+print('generated matrix & trace:', matrix, correct_trace)
 
-fitted_matrix = fit_matrix_linear(matrix, 2, 10)
+fitted_matrix = fit_matrix_linear(matrix, len(matrix), 10)
 fitted_matrix = t.unsqueeze(fitted_matrix, 0)
 fitted_matrix.requires_grad = True
 
+trace_abs_diff = []
 trace_diff_squares = []
 diagonal_grad_diff_squares = []
 non_diagonal_gard_diff_squares = []
@@ -28,10 +32,11 @@ for net_num in range(5, 301, 5):
     pred_trace.backward()
     g = fitted_matrix.grad.numpy()
     g = g[0]  # batch size is 1, take the 1st data
-    print(g)
+    # print(g)
 
     # collecting data
     trace_diff_squares.append((pred_trace - correct_trace) ** 2)
+    trace_abs_diff.append(abs(pred_trace - correct_trace))
 
     diag = 0
     non_diag = 0
@@ -49,6 +54,12 @@ for net_num in range(5, 301, 5):
 from matplotlib import pyplot as plt
 
 net_nums = [i for i in range(5, 301, 5)]
+
+plt.title('trace abs difference')
+plt.plot(net_nums, trace_abs_diff)
+plt.savefig('./data/trace abs difference.png')
+plt.show()
+plt.close()
 
 plt.title('trace square difference')
 plt.plot(net_nums, trace_diff_squares)
